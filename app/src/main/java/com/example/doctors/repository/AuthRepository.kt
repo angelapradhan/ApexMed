cf18fe761f734bbc3b7303ea0441829d0e2d9ffe
angelapradhan <angelapradhan123@gmail.com>
2026-03-01 13:35:40 +0545

    Auth Repo added

--- app/src/main/java/com/example/doctors/repository/AuthRepository.kt
+++ app/src/main/java/com/example/doctors/repository/AuthRepository.kt
@@ -3,20 +3,9 @@ package com.example.doctors.repository
 import com.example.doctors.model.User
 
 interface AuthRepository {
-    fun login(email: String, password: String,
-              callback: (Boolean, String)-> Unit)
-
-    fun register(
-        email: String, password: String,
-        callback: (Boolean, String, String) -> Unit
-    )
-
-    fun addUserToDatabase(userId: String, model: User,
-                          callback: (Boolean, String) -> Unit)
-
-    fun forgetPassword(email:String,callback: (Boolean, String) -> Unit)
-
-    fun getCurrentUser(callback: (Boolean, User?) -> Unit)
-
+    fun login(email: String, password: String, callback: (Boolean, String) -> Unit)
+    fun register(email: String, password: String, callback: (Boolean, String, String) -> Unit)
+    fun forgetPassword(email: String, callback: (Boolean, String) -> Unit)
+    fun logout()
     fun confirmPasswordReset(code: String, newPassword: String, callback: (Boolean, String?) -> Unit)
 }
